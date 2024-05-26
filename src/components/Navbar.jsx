12faae20ff2cabbc03c80e3da36de87017544ee2
kazimahin68 <kazimahin68@gmail.com>
2024-05-26 15:17:00 +0600

    primary setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,83 @@
+// import { useContext } from "react";
+import { Link } from "react-router-dom";
+// import { AuthContext } from "../AuthProvider/AuthProvider";
+
+const Navbar = () => {
+//   const { user, logOut } = useContext(AuthContext);
+
+  // console.log(user.photoURL);
+//   const handleLogout = () =>{
+//     logOut();
+//   }
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="">Dashboard</Link>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="dashboard">Dashboard</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        {/* {user ? (
+          <div className="flex items-center gap-2">
+            <div className="avatar online">
+              <div className="w-12 rounded-full">
+                <img src={user?.photoURL} />
+              </div>
+            </div>
+            <button onClick={handleLogout} className="btn bg-green-600 font-bold">Logout
+            </button>
+          </div>
+        ) : (
+          <button className="btn bg-green-600 font-bold">
+            <Link to="/login">Login</Link>
+          </button>
+        )} */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>This is my Home</h2>
+        </div>
+    );
+};
+
+export default Home;
